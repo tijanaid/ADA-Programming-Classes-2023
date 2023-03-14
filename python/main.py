b50df34009e4054879a26eabff8eb47f3b677c8a
devedzic <devedzic@gmail.com>
2023-03-15 00:06:29 +0100

    Project structure elaborated

---
+++ python/main.py
@@ -0,0 +1,47 @@
+"""The very first Python script - main.py.
+"""
+
+
+#%%
+# Hello world: the print() built-in function and the + operator.
+
+print('George Harrison')
+print('George Harrison', 'was born in', 1943, 'in Liverpool.')
+print('George Harrison', 'was born \n\tin', 1943, 'in Liverpool.')
+print('George Harrison ' + 'was born...')
+# print('George Harrison ' + 'was born in ' + 1943)
+print('George Harrison ' + 'was born in ' + str(1943))
+
+#%%
+# The input() built-in function
+
+
+
+#%%
+# A simple function and function call
+
+
+
+#%%
+# A simple loop and the range() built-in function
+
+
+
+#%%
+# A simple list, accessing list elements, printing lists
+
+
+
+#%%
+# Looping through list elements - for and enumerate()
+
+
+
+#%%
+# Global variables: __name__, __file__, __doc__,...
+
+
+
+#%%
+# Importing from another script
+
